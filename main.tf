68780658a35878dd71523b6412e4426cd7b622cd
Sophia Tang <Finenyaco@users.noreply.github.com>
2024-01-22 13:29:37 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,110 @@
+variable "test_str" {
+  type        = string
+  default     = "variable-test"
+}
+
+variable "test_num_without_type" {
+  default     = 123
+}
+
+variable "test_bool" {
+  type       = bool
+  default    = true
+}
+
+variable "test_list_number" {
+  type        = list(number)
+  default     = [80, 443]
+}
+
+variable "test_map_without_type" {
+  default     = {
+    name2      = "John"
+    age2      = "30"
+    email2     = "john@example.com"
+  }
+}
+
+variable "map_any" {
+  type        = map(any)
+  default     = {}
+}
+
+variable "list_map_string" {
+  type        = list(map(string))
+  description = "List of maps containing node pools"
+
+  default = [
+    {
+      name = "default-node-pool"
+    }
+  ]
+}
+
+variable "test_object_with_default_optional" {
+  type          = object({
+    name        = string
+    age         = number
+    email       = optional(object({
+        address = string
+        domain  = optional(string, "test.com")
+    }))
+  })
+  default       = {
+    name        = "Bob"
+    age         = 23
+    email       = {
+        address = "bob"
+        domain  = "example.com"
+    }
+  }
+}
+
+
+variable "test_list_str" {
+  description = "List string"
+  type = list(string)
+  default = ["Dec", "16"]
+}
+
+output "output_test_str" {
+  value         = var.test_str 
+}
+
+output "output_test_num_without_type" {
+  value         = var.test_num_without_type
+}
+
+output "output_test_bool" {
+  value         = var.test_bool
+}
+
+output "output_test_list_number" {
+  value         = var.test_list_number
+}
+
+
+output "output_test_map_without_type" {
+  value         = var.test_map_without_type
+}
+
+output "output_map_any" {
+   value       = var.map_any
+}
+
+output "output_list_map_string" {
+   value       = var.list_map_string
+}
+
+output "output_test_object_with_default_optional" {
+  value         = var.test_object_with_default_optional
+}
+
+
+output "output_test_list_str" {
+value           = var.test_list_str
+}
+
+output "endpoint_localhost" {
+  value         = "https://localhost"
+}
